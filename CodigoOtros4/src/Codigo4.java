3e7ea95b2f733b3f7c91259d1c5083d23fbbeb13
Juan SP <juan-220597@hotmail.com>
2024-09-27 17:17:43 -0600

    Se arreglo el codigo java, ya que tenia un error que siempre arrojaba el mismo ganador

--- CodigoOtros4/src/Codigo4.java
+++ CodigoOtros4/src/Codigo4.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Codigo4 {
 	//Se agrego el main para que pudiera correr el codigo
+@SuppressWarnings("resource")
 public static void main(String [] args) {
 //Se arreglaron los Scanners	
 Scanner s = new Scanner(System.in);
@@ -13,26 +14,29 @@ Scanner s = new Scanner(System.in);
     Scanner s2 = new Scanner(System.in);
     String j2 = s2.nextLine();
     //Se elimino un ) ya que estaba mal ahi en el if 
-    if (j1 == j2) {
+    if (j1.equals(j2)) {
       System.out.println("Empate");
     } else {
       int g = 2;
       switch(j1) {
         case "piedra":
-          if (j2 == "tijeras") {
+          if (j2.equals("tijeras")) {
             g = 1;
           }
-
+          break;//se agregan break para evitar flujo de codigo de manera incorrecta
         case "papel":
-          if (j2 == "piedra") {
+          if (j2.equals("piedra")) {
             g = 1;
           }//se agrego un } debido a que no estaba cerrado
+          break;
         case "tijera":
           if (j2.equals("papel")) {
             g = 1;
           }
           break;
-        default:
+        default://agregamos este sysout por su la jugada no es ninguna de las opciones
+        	 System.out.println("Movimiento no válido");
+             return; 
       }
       System.out.println("Gana el jugador " + g);
     }
@@ -40,4 +44,4 @@ Scanner s = new Scanner(System.in);
     s.close();
     s2.close();
 }//class Codigo4
-}
+}//main
